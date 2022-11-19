de8dbffb4f8ce09201a3a532c8f34a2a86fccba0
john-mirage <28307090+john-mirage@users.noreply.github.com>
2022-11-19 19:21:33 +0100

    fix: fix type check of the dispatch group and unit

--- src/helpers/type-checkers.js
+++ src/helpers/type-checkers.js
@@ -12,11 +12,13 @@ export const categoryIsValid = (category) => {
 
 export const groupIsValid = (group) => {
   return (
-    Object.keys(group).length === 3 &&
+    Object.keys(group).length === 4 &&
     group.hasOwnProperty("categoryId") &&
+    group.hasOwnProperty("categoryOrderId") &&
     group.hasOwnProperty("id") &&
     group.hasOwnProperty("size") &&
     typeof group.categoryId === "string" &&
+    typeof group.categoryOrderId === "string" &&
     typeof group.id === "string" &&
     typeof group.size === "string"
   );
@@ -24,15 +26,24 @@ export const groupIsValid = (group) => {
 
 export const unitIsValid = (unit) => {
   return (
-    Object.keys(unit).length === 6 &&
+    Object.keys(unit).length === 10 &&
+    unit.hasOwnProperty("hasCategory") &&
     unit.hasOwnProperty("categoryId") &&
+    unit.hasOwnProperty("categoryOrderId") &&
+    unit.hasOwnProperty("hasGroup") &&
     unit.hasOwnProperty("groupId") &&
+    unit.hasOwnProperty("groupOrderId") &&
     unit.hasOwnProperty("id") &&
     unit.hasOwnProperty("number") &&
     unit.hasOwnProperty("name") &&
     unit.hasOwnProperty("role") &&
-    typeof unit.categoryId === "string" &&
+    typeof unit.hasCategory === "boolean" &&
+    (typeof unit.categoryId === "string" || unit.categoryId === null) &&
+    (typeof unit.categoryOrderId === "string" ||
+      unit.categoryOrderId === null) &&
+    typeof unit.hasGroup === "boolean" &&
     (typeof unit.groupId === "string" || unit.groupId === null) &&
+    (typeof unit.groupOrderId === "string" || unit.groupOrderId === null) &&
     typeof unit.id === "string" &&
     typeof unit.number === "string" &&
     typeof unit.name === "string" &&
